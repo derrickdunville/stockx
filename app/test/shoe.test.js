3471166ae8be20c14cc605e73d9009327b83e5a6
Derrick Dunville <derrickdunville@gmail.com>
2019-08-08 20:12:35 -0700

    All test use promises now. Reverted docker-compose.yml changes.

--- app/test/shoe.test.js
+++ app/test/shoe.test.js
@@ -6,60 +6,61 @@ const chai      = require('chai'),
       should    = chai.should(),
       pool      = require('../src/utils/postgresPool'),
       init      = require('./init')
-      
+
 chai.use(chaiHttp)
 
 describe('Shoe Test Suite', () => {
 
   beforeEach((done) => {
     pool.query('DELETE FROM shoe', [])
-    .then(res => done())
-    .catch(err => console.error(err.stack))
+    .then(repsonse => done())
+    .catch(error => console.error(error.stack))
   })
 
   describe('POST /shoe', () => {
     it('it should post a shoe', (done) => {
       let testShoe = { name: "addidas Yeezy" }
-      chai.request(server)
-        .post('/shoe').send(testShoe).end((error, response) => {
-          response.should.have.status(200)
-          response.body.should.have.property('name')
-          response.body.name.should.equal('addidas Yeezy')
-          response.body.should.have.property('id')
-          done()
-        })
+      chai.request(server).post('/shoe').send(testShoe)
+      .then(response => {
+        response.should.have.status(200)
+        response.body.should.have.property('name')
+        response.body.name.should.equal('addidas Yeezy')
+        response.body.should.have.property('id')
+        done()
+      }).catch(error => {
+        console.error(error)
+      })
     })
 
     it('it should get duplicate key error', (done) => {
       let testShoe = { name: "addidas Yeezy" }
-      chai.request(server)
-        .post('/shoe').send(testShoe).end((error, response) => {
-          response.should.have.status(200)
-          response.body.should.have.property('name')
-          response.body.name.should.equal(testShoe.name)
-          response.body.should.have.property('id')
-        })
-      chai.request(server)
-        .post('/shoe').send(testShoe).end((error, response) => {
-          response.should.have.status(400)
-          response.body.should.have.property('err')
-          response.body.err.should.equal('duplicate key error')
-          done()
-        })
+      chai.request(server).post('/shoe').send(testShoe)
+      .then(response => {
+        response.should.have.status(200)
+        response.body.should.have.property('name')
+        response.body.name.should.equal(testShoe.name)
+        response.body.should.have.property('id')
+        return chai.request(server).post('/shoe').send(testShoe)
+      }).then(response => {
+        response.should.have.status(400)
+        response.body.should.have.property('err')
+        response.body.err.should.equal('duplicate key error')
+        done()
+      }).catch(error => {
+        console.error(error)
+      })
     })
     it('it should get not null violation', (done) => {
       let testShoe = {}
-      chai.request(server)
-        .post('/shoe').send(testShoe).end((error, response) => {
-          response.should.have.status(400)
-          response.body.should.have.property('err')
-          response.body.err.should.equal('not null violation')
-          done()
-        })
+      chai.request(server).post('/shoe').send(testShoe)
+      .then(response => {
+        response.should.have.status(400)
+        response.body.should.have.property('err')
+        response.body.err.should.equal('not null violation')
+        done()
+      }).catch(error => {
+        console.error(error)
+      })
     })
-  })
-
-  after((done) => {
-    done()
   })
 })
--- app/test/trueToSize.test.js
+++ app/test/trueToSize.test.js
@@ -15,14 +15,14 @@ describe('TrueToSize Test', () => {
 
   beforeEach((done) => {
     pool.query('DELETE FROM true_to_size', [])
-    .then(res => {
+    .then(response => {
       return pool.query('DELETE FROM shoe', [])
-    }).then(res => {
+    }).then(response => {
       return pool.query('INSERT INTO shoe (name) VALUES ($1) RETURNING *', ['addidas Yeezy'])
-    }).then(res => {
-      testShoe = res.rows[0]
+    }).then(response => {
+      testShoe = response.rows[0]
       done()
-    }).catch(err => console.error(err.stack))
+    }).catch(error => console.error(error.stack))
   })
 
   describe('POST /true_to_size', () => {
@@ -31,87 +31,99 @@ describe('TrueToSize Test', () => {
         shoe_id: testShoe.id,
         value: 1
       }
-      chai.request(server)
-        .post('/true_to_size').send(testTrueToSize).end((error, response) => {
-          response.should.have.status(200)
-          response.body.should.have.property('shoe_id')
-          response.body.shoe_id.should.equal(testTrueToSize.shoe_id)
-          response.body.should.have.property('value')
-          response.body.value.should.equal(testTrueToSize.value)
-          done()
-        })
+      chai.request(server).post('/true_to_size').send(testTrueToSize)
+      .then(response => {
+        response.should.have.status(200)
+        response.body.should.have.property('shoe_id')
+        response.body.shoe_id.should.equal(testTrueToSize.shoe_id)
+        response.body.should.have.property('value')
+        response.body.value.should.equal(testTrueToSize.value)
+        done()
+      }).catch(error => {
+        console.error(error)
+      })
     })
     it('it should get foreign key violation', (done) => {
       let testTrueToSize = {
         shoe_id: 2,
         value: 1
       }
-      chai.request(server)
-        .post('/true_to_size').send(testTrueToSize).end((error, response) => {
-          response.should.have.status(400)
-          response.body.should.have.property('err')
-          response.body.err.should.equal('foreign key violation')
-          done()
-        })
+      chai.request(server).post('/true_to_size').send(testTrueToSize)
+      .then(response => {
+        response.should.have.status(400)
+        response.body.should.have.property('err')
+        response.body.err.should.equal('foreign key violation')
+        done()
+      }).catch(error => {
+        console.error(error)
+      })
     })
     it('it should get constraint violation for value < 1', (done) => {
       let testTrueToSize = {
         shoe_id: testShoe.id,
         value: 0
       }
-      chai.request(server)
-        .post('/true_to_size').send(testTrueToSize).end((error, response) => {
-          response.should.have.status(400)
-          response.body.should.have.property('err')
-          response.body.err.should.equal('check violation')
-          done()
-        })
+      chai.request(server).post('/true_to_size').send(testTrueToSize)
+      .then(response => {
+        response.should.have.status(400)
+        response.body.should.have.property('err')
+        response.body.err.should.equal('check violation')
+        done()
+      }).catch(error => {
+        console.error(error)
+      })
     })
     it('it should get constraint violation for value > 5', (done) => {
       let testTrueToSize = {
         shoe_id: testShoe.id,
         value: 6
       }
-      chai.request(server)
-        .post('/true_to_size').send(testTrueToSize).end((error, response) => {
-          response.should.have.status(400)
-          response.body.should.have.property('err')
-          response.body.err.should.equal('check violation')
-          done()
-        })
+      chai.request(server).post('/true_to_size').send(testTrueToSize)
+      .then(response => {
+        response.should.have.status(400)
+        response.body.should.have.property('err')
+        response.body.err.should.equal('check violation')
+        done()
+      }).catch(error => {
+        console.error(error)
+      })
     })
     it('it should get non null violation for value', (done) => {
       let testTrueToSize = {
         shoe_id: testShoe.id,
       }
-      chai.request(server)
-        .post('/true_to_size').send(testTrueToSize).end((error, response) => {
+      chai.request(server).post('/true_to_size').send(testTrueToSize)
+      .then(response => {
           response.should.have.status(400)
           response.body.should.have.property('err')
           response.body.err.should.equal('not null violation')
           done()
-        })
+      }).catch(error => {
+        console.error(error)
+      })
     })
     it('it should get non null violation for shoe_id', (done) => {
       let testTrueToSize = {
         value: 1
       }
-      chai.request(server)
-        .post('/true_to_size').send(testTrueToSize).end((error, response) => {
-          response.should.have.status(400)
-          response.body.should.have.property('err')
-          response.body.err.should.equal('not null violation')
-          done()
-        })
+      chai.request(server).post('/true_to_size').send(testTrueToSize)
+      .then(response => {
+        response.should.have.status(400)
+        response.body.should.have.property('err')
+        response.body.err.should.equal('not null violation')
+        done()
+      }).catch(error => {
+        console.error(error)
+      })
     })
   })
 
   after((done) => {
     pool.query('DELETE FROM true_to_size', [])
-    .then(res => {
+    .then(response => {
       return pool.query('DELETE FROM shoe', [])
-    }).then(res => {
+    }).then(response => {
       done()
-    }).catch(err => console.error(err.stack))
+    }).catch(error => console.error(error.stack))
   })
 })
